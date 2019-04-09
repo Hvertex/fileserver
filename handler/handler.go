5d7aacccf7a4643e454af248427d55f735647b8f
liwei <lylwself@gmail.com>
2019-04-09 19:23:45 +0800

    init file server project

---
+++ handler/handler.go
@@ -0,0 +1,21 @@
+package handler
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func UploadFileHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		// 返回index.html
+		data, err := ioutil.ReadFile("./static/view/index.html")
+		if err != nil {
+			fmt.Printf("read file error, %v", err.Error())
+			return
+		}
+		w.Write(data)
+	} else {
+		// 执行上传
+	}
+}
